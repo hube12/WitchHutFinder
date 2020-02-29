977bfc4d204aac900bcbfae4adf33ff052d4a5b0
Neil <h66R10@gmail.com>
2020-02-29 17:31:07 +0100

    Forgot to change name of test

--- test/run.sh
+++ test/run.sh
@@ -1,4 +1,4 @@
-../TripleWitchHut 1.12 181201211981019340 100000
+../WitchHutFinder 1.12 181201211981019340 100000
 diff out.txt test.txt
 diff -s ../out.txt test.txt > /dev/null
 if [ $? -ne 0 ]; then
